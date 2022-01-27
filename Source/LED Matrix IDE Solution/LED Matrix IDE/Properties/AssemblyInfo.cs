efec458cd6349f870802f06d0a1080b9f088f8b5
Daniel Porrey <porrey@users.noreply.github.com>
2022-01-27 05:51:30 -0600

    Updated for Windows 11

--- Source/LED Matrix IDE Solution/LED Matrix IDE/Properties/AssemblyInfo.cs
+++ Source/LED Matrix IDE Solution/LED Matrix IDE/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Daniel M. Porrey")]
 [assembly: AssemblyProduct("LedMatrixIde")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © 2018-2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 [assembly: ComVisible(false)]
